fd8348e148806131c261c41fe2e8b7aa7a225701
bilalaymak <bilalaymak@gmail.com>
2023-03-17 17:30:20 +0300

    JDBC updated

--- src/main/java/PreparedStatement01.java
+++ src/main/java/PreparedStatement01.java
@@ -9,9 +9,7 @@ public class PreparedStatement01 {
         //--1.Example: Update the number of employees to 9999
         // if the company name is IBM by using prepared statement
         //1. step: create prepared statement query
-        String sql1 = " update companies\n" +
-                " set number_of_employees =?/\n" +
-                " where company = ?";
+        String sql1 = "UPDATE companies SET number_of_employees = ? WHERE company = ?";
 
         //2. step: create prepared statement object
         PreparedStatement ps1 = connection.prepareStatement(sql1);
@@ -20,6 +18,24 @@ public class PreparedStatement01 {
 
         ps1.setInt(1,9999);
         ps1.setString(2,"IBM");
+
+        //4. step: execute the query for update
+        int numOfRows = ps1.executeUpdate();
+        System.out.println("updated : " + numOfRows);
+        String sql2 = "select * from companies";
+
+        //5. step : display the result
+
+        Statement statement1 = connection.createStatement();
+        ResultSet rs2 = statement1.executeQuery(sql2);
+
+        while (rs2.next()){
+            System.out.println(
+                    rs2.getInt(1) + " : " +
+                    rs2.getString(2) + " : " +
+                    rs2.getInt(3)
+            );
+        }
 
 
 
---
+++ src/main/java/practice02/CallableStatement01.java
@@ -0,0 +1,4 @@
+package practice02;
+
+public class CallableStatement01 {
+}
